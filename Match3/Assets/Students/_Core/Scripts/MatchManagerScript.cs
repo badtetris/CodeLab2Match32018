2b07aaa18a4d67d3acfbe8abb64ced97fe6dffc1
Amanda Ekaratih Siswojo <aes793@nyu.edu>
2018-09-21 12:40:25 -0400

    Vertical Match
    Tried to fix but did not fix.

--- Match3/Assets/Students/_Core/Scripts/MatchManagerScript.cs
+++ Match3/Assets/Students/_Core/Scripts/MatchManagerScript.cs
@@ -17,6 +17,12 @@ public class MatchManagerScript : MonoBehaviour {
 				if(x < gameManager.gridWidth - 2){
 					match = match || GridHasHorizontalMatch(x, y);
 				}
+
+                if(y < gameManager.gridHeight - 2)
+                {
+                    match = match || GridHasVerticalMatch(x, y);
+                }
+                
 			}
 		}
 		
@@ -39,7 +45,27 @@ public class MatchManagerScript : MonoBehaviour {
 		}
 	}
 
-	public int GetHorizontalMatchLength(int x, int y){
+    public bool GridHasVerticalMatch(int x, int y)
+    {
+        GameObject token1 = gameManager.gridArray[x, y + 0];
+        GameObject token2 = gameManager.gridArray[x, y + 1];
+        GameObject token3 = gameManager.gridArray[x, y + 2];
+
+        if (token1 != null && token2 != null && token3 != null)
+        {
+            SpriteRenderer sr1 = token1.GetComponent<SpriteRenderer>();
+            SpriteRenderer sr2 = token2.GetComponent<SpriteRenderer>();
+            SpriteRenderer sr3 = token3.GetComponent<SpriteRenderer>();
+
+            return (sr1.sprite == sr2.sprite && sr2.sprite == sr3.sprite);
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public int GetHorizontalMatchLength(int x, int y){
 		int matchLength = 1;
 		
 		GameObject first = gameManager.gridArray[x, y];
@@ -67,7 +93,44 @@ public class MatchManagerScript : MonoBehaviour {
 		return matchLength;
 	}
 
-	public virtual int RemoveMatches(){
+    public int GetVerticalMatchLength(int x, int y)
+    {
+        int matchLength = 1;
+
+        GameObject first = gameManager.gridArray[x, y];
+
+        if (first != null)
+        {
+            SpriteRenderer sr1 = first.GetComponent<SpriteRenderer>();
+
+            for (int i = y + 1; i < gameManager.gridHeight; i++)
+            {
+                GameObject other = gameManager.gridArray[i, y];
+
+                if (other != null)
+                {
+                    SpriteRenderer sr2 = other.GetComponent<SpriteRenderer>();
+
+                    if (sr1.sprite == sr2.sprite)
+                    {
+                        matchLength++;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+
+        return matchLength;
+    }
+
+    public virtual int RemoveMatches(){
 		int numRemoved = 0;
 
 		for(int x = 0; x < gameManager.gridWidth; x++){
